186c8b53b727bf5ec3a7656a77a1b3d073e0385e
Jean <theil@users.noreply.github.com>
2016-10-08 18:31:54 -0400

    Tweaks

--- src/js/bytesauce-carousel.js
+++ src/js/bytesauce-carousel.js
@@ -1,3 +1,34 @@
+//Requirements:
+//
+//    - build a carousel.
+//- by default display 4 elements.
+//- navigation is made with next and previous buttons.
+//- Next button should be disabled, if user is at the very end of the carousel.
+//- Previous button should be disabled, if user is at the very beginning of carousel.
+//- Clicking on next/previous buttons shows next/prev 4 carousel images accordingly.
+//- The data source of carousel use a variable with a data structure similar to this JSON:
+//
+//    [
+//        {
+//            title: 'First Block',
+//            images: [{title:'image1 title', url: 'url'}, {title:'image2 title', url: 'url'}, {title:'image3 title', url: 'url'}]
+//        },
+//        {
+//            title: 'Second Block',
+//            images: [{title:'image20 title', url: 'url'}, {title:'image23 title', url: 'url'}]
+//        }
+//        ,
+//        {
+//            title: 'Third Block',
+//            images: [{title:'image7 title', url: 'url'}]
+//        }
+//    ]
+//
+//Note: Display random images using lorempixel.com
+//
+//Please use AngularJS 1.5 and try to avoid Jquery.
+
+
 angular.module('bytesauceCarousel', [])
 
     .constant('VERSION', '0.1.0')
@@ -10,8 +41,22 @@ angular.module('bytesauceCarousel', [])
 
     })
 
-    .directive('directiveName', function () {
+    .directive('bytesauceCarousel', function () {
+        return {
+            restrict: 'E',
+            template: "<h1>Hello</h1>",
+            compile : function(element, attributes){
+                element.css("border", "1px solid #cccccc");
 
+                var linkFunction = function($scope, element, attributes) {
+                    console.log($scope.items, "ITEMS");
+                    element.html("Student: <b>" + $scope.items[0] + "</b><br/>");
+                    element.css("background-color", "#ff00ff");
+                };
+                return linkFunction;
+
+            }
+        };
     })
 ;
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 var app = angular.module("carouselDemo", ['bytesauceCarousel']);
 app.controller("appCtrl", function($scope) {
-    $scope.items = ["item 1", "item 2", "item 3"];
+    $scope.items = [
+        {
+            title: 'First Block',
+            images: [{title:'image1 title', url: 'http://lorempixel.com/400/200'}, {title:'image2 title', url: 'http://lorempixel.com/400/200'}, {title:'image3 title', url: 'http://lorempixel.com/400/200'}]
+        },
+        {
+            title: 'Second Block',
+            images: [{title:'image20 title', url: 'http://lorempixel.com/400/200'}, {title:'image23 title', url: 'http://lorempixel.com/400/200'}]
+        }
+        ,
+        {
+            title: 'Third Block',
+            images: [{title:'image7 title', url: 'http://lorempixel.com/400/200'}]
+        }
+    ];
 });
 
